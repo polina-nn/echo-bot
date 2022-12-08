f2c718162d4a8b62e28dc9b391d784cb219a1ad0
Polina <morozova_p_v@mail.ru>
2022-12-08 17:07:45 +0300

    I didn't solve right the conflict in last merge. Fixed it.

--- src/FrontEnd/TelegramAPI.hs
+++ src/FrontEnd/TelegramAPI.hs
@@ -5,14 +5,15 @@
 -- | TelegramAPI has function for http requests to API Telegram  (type IO)
 -- and some service functions for return required record fields of TgUpdate, TgMessage
 module FrontEnd.TelegramAPI
-  ( getMeTg
-  , getTgUpdates
-  , tgGetLastUpdateAndId
-  , checkEvent
-  , sendTgResponse
-  , tgGetLastMessageOrCallback
-  , chooseCurrentChatHandle
-  ) where
+  ( getMeTg,
+    getTgUpdates,
+    tgGetLastUpdateAndId,
+    checkEvent,
+    sendTgResponse,
+    tgGetLastMessageOrCallback,
+    chooseCurrentChatHandle,
+  )
+where
 
 import qualified Config
 import qualified Control.Exception.Safe as EX
@@ -35,9 +36,9 @@ import qualified Text.Read
 
 -- | buildRequestParams -- create QueryString for request
 buildRequestParams ::
-     (Req.QueryParam p, Monoid p)
-  => [(TgTypes.TgQueryParam, TgTypes.TgValueParam)]
-  -> p
+  (Req.QueryParam p, Monoid p) =>
+  [(TgTypes.TgQueryParam, TgTypes.TgValueParam)] ->
+  p
 buildRequestParams [] = mempty
 buildRequestParams params = mconcat $ fmap (uncurry (Req.=:)) params
 
@@ -46,55 +47,56 @@ buildRequestParams params = mconcat $ fmap (uncurry (Req.=:)) params
 getMeTg :: TgTypes.Token -> IO TgTypes.TgUser
 getMeTg t =
   EX.handle TgException.rethrowReqException $
-  MIO.liftIO $
-  Req.runReq Req.defaultHttpConfig $ do
-    r <-
-      Req.req
-        Req.GET
-        (Req.https "api.telegram.org" Req./: T.pack ("bot" ++ t) Req./: "getMe")
-        Req.NoReqBody
-        Req.jsonResponse
-        mempty
-    return $ (TgTypes.tgGetMeResponseResult . Req.responseBody) r
+    MIO.liftIO $
+      Req.runReq Req.defaultHttpConfig $ do
+        r <-
+          Req.req
+            Req.GET
+            (Req.https "api.telegram.org" Req./: T.pack ("bot" ++ t) Req./: "getMe")
+            Req.NoReqBody
+            Req.jsonResponse
+            mempty
+        return $ (TgTypes.tgGetMeResponseResult . Req.responseBody) r
 
 -- | getTgUpdates -- get updates. If it return IO Nothig -- last TgUpdate is empty
 -- when you call getTgUpdates with (Just lastUpdateId),
 -- you call whith lastUpdateId+1, because you are waiting new update.
 -- If internet is invalid, getTgUpdates send  Exception!
 getTgUpdates ::
-     TgTypes.Token -> Maybe TgTypes.UpdateId -> IO (Maybe [TgTypes.TgUpdate])
+  TgTypes.Token -> Maybe TgTypes.UpdateId -> IO (Maybe [TgTypes.TgUpdate])
 getTgUpdates token Nothing = getTgUpdatesHelp token []
 getTgUpdates token (Just lastUpdateId) =
   getTgUpdatesHelp
     token
-    [ ("offset", T.pack $ show $ lastUpdateId + 1)
-    , ("timeout", T.pack $ show (10 :: Int))
+    [ ("offset", T.pack $ show $ lastUpdateId + 1),
+      ("timeout", T.pack $ show (10 :: Int))
     ]
 
 getTgUpdatesHelp ::
-     TgTypes.Token
-  -> [(TgTypes.TgQueryParam, TgTypes.TgValueParam)]
-  -> IO (Maybe [TgTypes.TgUpdate])
+  TgTypes.Token ->
+  [(TgTypes.TgQueryParam, TgTypes.TgValueParam)] ->
+  IO (Maybe [TgTypes.TgUpdate])
 getTgUpdatesHelp token params =
   EX.handle TgException.rethrowReqException $
-  MIO.liftIO $
-  Req.runReq Req.defaultHttpConfig $ do
-    r <-
-      Req.req
-        Req.GET
-        (Req.https "api.telegram.org" Req./: T.pack ("bot" ++ token) Req./:
-         "getUpdates")
-        Req.NoReqBody
-        Req.jsonResponse
-        (buildRequestParams params)
-    return $
-      TgTypes.tgGetUpdateResponseBodyResult <$>
-      Data.Aeson.Types.parseMaybe
-        A.parseJSON
-        (Req.responseBody response :: A.Value)
+    MIO.liftIO $
+      Req.runReq Req.defaultHttpConfig $ do
+        r <-
+          Req.req
+            Req.GET
+            ( Req.https "api.telegram.org" Req./: T.pack ("bot" ++ token)
+                Req./: "getUpdates"
+            )
+            Req.NoReqBody
+            Req.jsonResponse
+            (buildRequestParams params)
+        return $
+          TgTypes.tgGetUpdateResponseBodyResult
+            <$> Data.Aeson.Types.parseMaybe
+              A.parseJSON
+              (Req.responseBody r :: A.Value)
 
 tgGetLastUpdateAndId ::
-     Maybe [TgTypes.TgUpdate] -> Maybe (TgTypes.TgUpdate, Int)
+  Maybe [TgTypes.TgUpdate] -> Maybe (TgTypes.TgUpdate, Int)
 tgGetLastUpdateAndId Nothing = Nothing
 tgGetLastUpdateAndId (Just []) = Nothing
 tgGetLastUpdateAndId (Just results) =
@@ -127,12 +129,12 @@ checkEvent :: TgTypes.MessageOrCallback -> EchoBot.Event TgTypes.Content
 checkEvent (TgTypes.Callback mess _) = checkCallbackQuery mess
   where
     checkCallbackQuery ::
-         TgTypes.TgCallbackQuery -> EchoBot.Event TgTypes.Content
+      TgTypes.TgCallbackQuery -> EchoBot.Event TgTypes.Content
     checkCallbackQuery callBack =
       case checkCallbackFrom1To5 callBack of
         Nothing ->
           EchoBot.MessageEvent $
-          TgTypes.ErrorMessage "You entered something, but numbers from 1 to 5 "
+            TgTypes.ErrorMessage "You entered something, but numbers from 1 to 5 "
         Just number -> EchoBot.SetRepetitionCountEvent number
 checkEvent TgTypes.ErrorAPI =
   EchoBot.MessageEvent $ TgTypes.ErrorMessage "Error API Telegram!"
@@ -162,10 +164,10 @@ checkCallbackFrom1To5 TgTypes.TgCallbackQuery {tgCallbackQueryData = Just val} =
 
 -- | chooseCurrentChatHandle - choose/create handle for received message/callback.
 chooseCurrentChatHandle ::
-     EchoBot.Handle IO TgTypes.Content
-  -> TgTypes.MessageOrCallback
-  -> IORef TgTypes.TgRepeats
-  -> IO (Maybe TgTypes.Handle)
+  EchoBot.Handle IO TgTypes.Content ->
+  TgTypes.MessageOrCallback ->
+  IORef TgTypes.TgRepeats ->
+  IO (Maybe TgTypes.Handle)
 chooseCurrentChatHandle h (TgTypes.Message _ chat) mapRepeats =
   chooseCurrentChatHandle' h chat mapRepeats
 chooseCurrentChatHandle h (TgTypes.Callback _ (Just chat)) mapRepeats =
@@ -175,10 +177,10 @@ chooseCurrentChatHandle _ TgTypes.ErrorAPI _ = return Nothing
 
 -- | chooseCurrentChatHandle' -- choose/create handle for received message/callback. Create handle from template in makeBotHandle
 chooseCurrentChatHandle' ::
-     EchoBot.Handle IO TgTypes.Content
-  -> TgTypes.ChatId
-  -> IORef TgTypes.TgRepeats
-  -> IO (Maybe TgTypes.Handle)
+  EchoBot.Handle IO TgTypes.Content ->
+  TgTypes.ChatId ->
+  IORef TgTypes.TgRepeats ->
+  IO (Maybe TgTypes.Handle)
 chooseCurrentChatHandle' h chat mapRepeats = do
   mapRepeats' <- readIORef mapRepeats
   case Map.lookup chat mapRepeats' of
@@ -188,10 +190,10 @@ chooseCurrentChatHandle' h chat mapRepeats = do
       Logger.logDebug (EchoBot.hLogHandle h) $
         T.concat
           [ T.pack
-              "chooseCurrentChatHandle: OK! New handle with state from config "
-          , T.pack $ show state
-          , T.pack "for new chart Id"
-          , T.pack $ show chat
+              "chooseCurrentChatHandle: OK! New handle with state from config ",
+            T.pack $ show state,
+            T.pack "for new chart Id",
+            T.pack $ show chat
           ]
       let newHandle' = TgTypes.Handle {hBotHandle = newHandle}
       let fun = Map.insert chat newHandle'
@@ -202,53 +204,53 @@ chooseCurrentChatHandle' h chat mapRepeats = do
       state <- EchoBot.hGetState (TgTypes.hBotHandle val)
       Logger.logDebug (EchoBot.hLogHandle h) $
         T.concat
-          [ T.pack "chooseCurrentChatHandle: OK! Handle with state  "
-          , T.pack $ show state
-          , T.pack "for chart Id"
-          , T.pack $ show chat
+          [ T.pack "chooseCurrentChatHandle: OK! Handle with state  ",
+            T.pack $ show state,
+            T.pack "for chart Id",
+            T.pack $ show chat
           ]
       return $ Just val
 
 -- makeBotHandle -- clean handle whith stateRef from config.
 makeBotHandle ::
-     EchoBot.Handle IO TgTypes.Content -> IO (EchoBot.Handle IO TgTypes.Content)
+  EchoBot.Handle IO TgTypes.Content -> IO (EchoBot.Handle IO TgTypes.Content)
 makeBotHandle h = do
   botConfig <- Config.getBotConfig
   initialState <- either (die . T.unpack) pure $ EchoBot.makeState botConfig
   stateRef <- newIORef initialState
   pure
     EchoBot.Handle
-      { EchoBot.hGetState = readIORef stateRef
-      , EchoBot.hModifyState' = modifyIORef' stateRef
-      , EchoBot.hLogHandle = EchoBot.hLogHandle h
-      , EchoBot.hConfig = botConfig
-      , EchoBot.hTextFromMessage =
+      { EchoBot.hGetState = readIORef stateRef,
+        EchoBot.hModifyState' = modifyIORef' stateRef,
+        EchoBot.hLogHandle = EchoBot.hLogHandle h,
+        EchoBot.hConfig = botConfig,
+        EchoBot.hTextFromMessage =
           \case
             TgTypes.ValidMessage text -> Just text
             TgTypes.ErrorMessage text -> Just text
             TgTypes.ErrorAPITelegram text -> Just text
-            TgTypes.Sticker text -> Just text
-      , EchoBot.hMessageFromText = TgTypes.ValidMessage
+            TgTypes.Sticker text -> Just text,
+        EchoBot.hMessageFromText = TgTypes.ValidMessage
       }
 
 sendTgResponse ::
-     EchoBot.Handle IO TgTypes.Content
-  -> TgTypes.Token
-  -> TgTypes.MessageOrCallback
-  -> [EchoBot.Response TgTypes.Content]
-  -> IO ()
-sendTgResponse h token (TgTypes.Message tgMes _) resp@((EchoBot.MessageResponse (TgTypes.ValidMessage val)):_) =
+  EchoBot.Handle IO TgTypes.Content ->
+  TgTypes.Token ->
+  TgTypes.MessageOrCallback ->
+  [EchoBot.Response TgTypes.Content] ->
+  IO ()
+sendTgResponse h token (TgTypes.Message tgMes _) resp@((EchoBot.MessageResponse (TgTypes.ValidMessage val)) : _) =
   sendTgText h (length resp) token tgMes val
-sendTgResponse h token (TgTypes.Message tgMes _) ((EchoBot.MessageResponse (TgTypes.ErrorMessage val)):_) =
+sendTgResponse h token (TgTypes.Message tgMes _) ((EchoBot.MessageResponse (TgTypes.ErrorMessage val)) : _) =
   sendTgText h 1 token tgMes val
-sendTgResponse h token (TgTypes.Message tgMes _) resp@((EchoBot.MessageResponse (TgTypes.Sticker stikerId)):_) =
+sendTgResponse h token (TgTypes.Message tgMes _) resp@((EchoBot.MessageResponse (TgTypes.Sticker stikerId)) : _) =
   sendTgStiker h (length resp) token tgMes stikerId
-sendTgResponse h token (TgTypes.Message tgMes _) ((EchoBot.MenuResponse title _):_) =
+sendTgResponse h token (TgTypes.Message tgMes _) ((EchoBot.MenuResponse title _) : _) =
   sendTgKeyboard h token tgMes title
 sendTgResponse h token (TgTypes.Callback call (Just _)) [] =
   sendTgAnswerCallbackQuery h token call
 sendTgResponse _ _ (TgTypes.Callback _ Nothing) [] = pure ()
-sendTgResponse h _ _ ((EchoBot.MessageResponse (TgTypes.ErrorAPITelegram _)):_) = do
+sendTgResponse h _ _ ((EchoBot.MessageResponse (TgTypes.ErrorAPITelegram _)) : _) = do
   Logger.logError (EchoBot.hLogHandle h) "sendTgResponse: BAD! ErrorAPITelegram"
   pure ()
 sendTgResponse h _ _ _ = do
@@ -256,12 +258,12 @@ sendTgResponse h _ _ _ = do
   pure ()
 
 sendTgText ::
-     EchoBot.Handle IO TgTypes.Content
-  -> EchoBot.RepetitionCount
-  -> TgTypes.Token
-  -> TgTypes.TgMessage
-  -> T.Text
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  EchoBot.RepetitionCount ->
+  TgTypes.Token ->
+  TgTypes.TgMessage ->
+  T.Text ->
+  IO ()
 sendTgText h repeats token tgMes text = do
   let chatId = TgTypes.tgChatId $ TgTypes.tgMessageChat tgMes
   sendTgMessage
@@ -272,12 +274,12 @@ sendTgText h repeats token tgMes text = do
     [("chat_id", T.pack $ show chatId), ("text", text)]
 
 sendTgStiker ::
-     EchoBot.Handle IO TgTypes.Content
-  -> EchoBot.RepetitionCount
-  -> TgTypes.Token
-  -> TgTypes.TgMessage
-  -> T.Text
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  EchoBot.RepetitionCount ->
+  TgTypes.Token ->
+  TgTypes.TgMessage ->
+  T.Text ->
+  IO ()
 sendTgStiker h repeats token tgMes stiker = do
   let chatId = TgTypes.tgChatId $ TgTypes.tgMessageChat tgMes
   sendTgMessage
@@ -288,21 +290,21 @@ sendTgStiker h repeats token tgMes stiker = do
     [("chat_id", T.pack $ show chatId), ("sticker", stiker)]
 
 sendTgKeyboard ::
-     EchoBot.Handle IO TgTypes.Content
-  -> TgTypes.Token
-  -> TgTypes.TgMessage
-  -> EchoBot.Title
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  TgTypes.Token ->
+  TgTypes.TgMessage ->
+  EchoBot.Title ->
+  IO ()
 sendTgKeyboard h token tgMes title = do
   let chatId = TgTypes.tgChatId $ TgTypes.tgMessageChat tgMes
   let keyboard :: TgTypes.TgInlineKeyboardMarkup
       keyboard =
         TgTypes.TgInlineKeyboardMarkup
-          [ [TgTypes.TgInlineKeyboardButton "1" "1"]
-          , [TgTypes.TgInlineKeyboardButton "2" "2"]
-          , [TgTypes.TgInlineKeyboardButton "3" "3"]
-          , [TgTypes.TgInlineKeyboardButton "4" "4"]
-          , [TgTypes.TgInlineKeyboardButton "5" "5"]
+          [ [TgTypes.TgInlineKeyboardButton "1" "1"],
+            [TgTypes.TgInlineKeyboardButton "2" "2"],
+            [TgTypes.TgInlineKeyboardButton "3" "3"],
+            [TgTypes.TgInlineKeyboardButton "4" "4"],
+            [TgTypes.TgInlineKeyboardButton "5" "5"]
           ]
       a = BC.unpack (A.encode keyboard)
   sendTgMessage
@@ -310,24 +312,24 @@ sendTgKeyboard h token tgMes title = do
     1
     token
     "sendMessage"
-    [ ("chat_id", T.pack $ show chatId)
-    , ("text", title)
-    , ("reply_markup", T.pack a)
+    [ ("chat_id", T.pack $ show chatId),
+      ("text", title),
+      ("reply_markup", T.pack a)
     ]
 
 sendTgAnswerCallbackQuery ::
-     EchoBot.Handle IO TgTypes.Content
-  -> TgTypes.Token
-  -> TgTypes.TgCallbackQuery
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  TgTypes.Token ->
+  TgTypes.TgCallbackQuery ->
+  IO ()
 sendTgAnswerCallbackQuery h token callbackQuery = do
   sendTgMessage
     h
     1
     token
     "answerCallbackQuery"
-    [ ("callback_query_id", T.pack $ TgTypes.tgCallbackQueryId callbackQuery)
-    , ("text", T.pack "Your reply has been received")
+    [ ("callback_query_id", T.pack $ TgTypes.tgCallbackQueryId callbackQuery),
+      ("text", T.pack "Your reply has been received")
     ]
   case TgTypes.tgCallbackQueryMessage callbackQuery :: Maybe TgTypes.TgMessage of
     Nothing -> do
@@ -350,47 +352,49 @@ sendTgAnswerCallbackQuery h token callbackQuery = do
             1
             token
             "editMessageReplyMarkup"
-            [ ("chat_id", T.pack $ show chatId)
-            , ("message_id", T.pack $ show messageId)
+            [ ("chat_id", T.pack $ show chatId),
+              ("message_id", T.pack $ show messageId)
             ]
           sendTgMessage
             h
             1
             token
             "sendMessage"
-            [ ("chat_id", T.pack $ show chatId)
-            , ( "text"
-              , T.pack $
-                "Answer received! Number of repetitions " ++ show number)
+            [ ("chat_id", T.pack $ show chatId),
+              ( "text",
+                T.pack $
+                  "Answer received! Number of repetitions " ++ show number
+              )
             ]
           _ <- EchoBot.respond h (EchoBot.SetRepetitionCountEvent number)
           pure ()
 
 -- | sendTgMessage -- helper function for sending messages
 sendTgMessage ::
-     EchoBot.Handle IO TgTypes.Content
-  -> EchoBot.RepetitionCount
-  -> TgTypes.Token
-  -> TgTypes.TgUrl
-  -> [(TgTypes.TgQueryParam, TgTypes.TgValueParam)]
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  EchoBot.RepetitionCount ->
+  TgTypes.Token ->
+  TgTypes.TgUrl ->
+  [(TgTypes.TgQueryParam, TgTypes.TgValueParam)] ->
+  IO ()
 sendTgMessage h repeats token url params =
   EX.handle TgException.rethrowReqException $
-  MIO.liftIO $
-  Req.runReq Req.defaultHttpConfig $ do
-    response <-
-      Control.Monad.replicateM repeats $
-      Req.req
-        Req.POST
-        (Req.https "api.telegram.org" Req./: T.pack ("bot" ++ token) Req./: url)
-        Req.NoReqBody
-        Req.jsonResponse
-        (buildRequestParams params)
-    Control.Monad.IO.Class.liftIO $ ckeckSendTgResponse h response
+    MIO.liftIO $
+      Req.runReq Req.defaultHttpConfig $ do
+        response <-
+          Control.Monad.replicateM repeats $
+            Req.req
+              Req.POST
+              (Req.https "api.telegram.org" Req./: T.pack ("bot" ++ token) Req./: url)
+              Req.NoReqBody
+              Req.jsonResponse
+              (buildRequestParams params)
+        MIO.liftIO $ ckeckSendTgResponse h response
+
 ckeckSendTgResponse ::
-     EchoBot.Handle IO TgTypes.Content
-  -> [Req.JsonResponse TgTypes.TgSendResponse]
-  -> IO ()
+  EchoBot.Handle IO TgTypes.Content ->
+  [Req.JsonResponse TgTypes.TgSendResponse] ->
+  IO ()
 ckeckSendTgResponse h [] = do
   Logger.logError
     (EchoBot.hLogHandle h)
